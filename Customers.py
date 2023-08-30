3bb6f2c72ea63d48835228a0637325127cfb299b
Linet <muthiigathoni@gmail.com>
2023-08-30 14:58:49 +0300

    Complete

--- Customers.py
+++ Customers.py
@@ -1,3 +1,5 @@
+from Reviews import Review
+from Restaurant import Restaurant
 class Customer:
 
     all_customers = []
@@ -7,6 +9,7 @@ class Customer:
         self._family_name = family_name
         self.__class__.all_customers.append(self)
         self._reviews = []
+        print(f"Created customer: {self.full_name()}")
 
     def given_name(self):
         return self._given_name
@@ -26,6 +29,7 @@ class Customer:
         review = Review(self, restaurant, rating)
         self._reviews.append(review)
         restaurant.add_review(review)
+        print(f"Added review for {restaurant.name()} by {self.full_name()}")
 
     def restaurants(self):
         reviewed_restaurants = set()
@@ -33,13 +37,13 @@ class Customer:
             reviewed_restaurants.add(review.restaurant())
         return list(reviewed_restaurants)
 
-# Aggregate and Association Methods
+    # Aggregate and Association Methods
     def num_reviews(self):
         return len(self._reviews)
 
     @classmethod
     def find_by_name(cls, full_name):
-        for customer in cls._all_customers:
+        for customer in cls.all_customers:
             if customer.full_name() == full_name:
                 return customer
         return None
@@ -47,8 +51,20 @@ class Customer:
     @classmethod
     def find_all_by_given_name(cls, given_name):
         matching_customers = []
-        for customer in cls._all_customers:
+        for customer in cls.all_customers:
             if customer.given_name() == given_name:
                 matching_customers.append(customer)
         return matching_customers
+
+
+# Example usage
+customer1 = Customer("Linet", "Muthii")
+customer2 = Customer("Joy", "Brittany")
+rest1 = Restaurant("Kijiji")
+customer1.add_review(rest1, 4)
+# customer2.add_review("Restaurant B", 5)
+
+print(Customer.find_by_name("Linet Muthii"))
+print(Customer.find_all_by_given_name("Joy"))
+print(customer1.given_name)
 
--- Restaurant.py
+++ Restaurant.py
@@ -2,7 +2,7 @@ class Restaurant:
     def __init__(self, name):
         self._name = name
         self._reviews = []
-
+        print(f"Created restaurant: {self._name}")
 
     @property
     def name(self):
@@ -13,8 +13,9 @@ class Restaurant:
         print("Error: Cannot change restaurant name.")
 
     # Object Relationship Methods
-def add_review(self, review):
+    def add_review(self, review):
         self._reviews.append(review)
+        print(f"Added review for {self._name}")
 
     def reviews(self):
         return self._reviews
@@ -25,10 +26,24 @@ def add_review(self, review):
             unique_customers.add(review.customer())
         return list(unique_customers)
 
-
-# Aggregate and Association Methods
- def average_star_rating(self):
+    # Aggregate and Association Methods
+    def average_star_rating(self):
         if len(self._reviews) == 0:
             return 0
         total_ratings = sum(review.rating() for review in self._reviews)
-        return total_ratings / len(self._reviews)
+        avg_rating = total_ratings / len(self._reviews)
+        print(f"Average star rating for {self._name}: {avg_rating}")
+        return avg_rating
+
+
+# Example usage
+restaurant1 = Restaurant("Restaurant A")
+restaurant2 = Restaurant("Restaurant B")
+restaurant1.add_review("Review A")
+restaurant2.add_review("Review B")
+
+print(restaurant1.name)
+print(restaurant2.name)
+restaurant1.name = "New Name"
+print(restaurant1.name)
+print(restaurant1.average_star_rating())
